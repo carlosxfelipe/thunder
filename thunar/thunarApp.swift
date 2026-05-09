6b8b84004470011d50e4640e795f65f0c3181cd7
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2026-05-09 16:37:33 -0300

    First commit

---
+++ thunar/thunarApp.swift
@@ -0,0 +1,17 @@
+//
+//  thunarApp.swift
+//  thunar
+//
+//  Created by Carlos Felipe Araújo on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct thunarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
